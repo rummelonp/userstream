69bf8cd55487a887d86b3a272acdbb4921cda81f
Kazuya Takeshima <mail@mitukiii.jp>
2011-03-30 22:17:32 +0900

    Override method_missing and respond_to?.

--- lib/userstream.rb
+++ lib/userstream.rb
@@ -6,4 +6,13 @@ class Userstream
     @access_token = access_token
     @rubytter = OAuthRubytter.new(@access_token)
   end
+
+  def method_missing(method, *args, &block)
+    return super unless @rubytter.respond_to?(method)
+    @rubytter.send(method, *args, &block)
+  end
+
+  def respond_to?(method)
+    return @rubytter.respond_to?(method) || super
+  end
 end

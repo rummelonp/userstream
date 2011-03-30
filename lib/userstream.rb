89fc6725252777cf5ecb8b0de065283717601f1e
Kazuya Takeshima <mail@mitukiii.jp>
2011-03-30 21:55:01 +0900

    Implement Userstream.initialize.

--- lib/userstream.rb
+++ lib/userstream.rb
@@ -1,4 +1,9 @@
 require 'rubytter'
 
 class Userstream
+  def initialize(consumer, access_token)
+    @consumer = consumer
+    @access_token = access_token
+    @rubytter = OAuthRubytter.new(@access_token)
+  end
 end

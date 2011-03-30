5dbafcbdbaa124d13e96a5314e9d3e3f687c2d00
Kazuya Takeshima <mail@mitukiii.jp>
2011-03-30 21:51:48 +0900

    Userstream.new spec.

--- spec/userstream_spec.rb
+++ spec/userstream_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Userstream" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Userstream do
+  before do
+    @consumer = OAuth::Consumer.new('consumer key', 'consumer secret')
+    @access_token = OAuth::AccessToken.new(@consumer, 'oauth token', 'oauth token secret')
+    @userstream = Userstream.new(@consumer, @access_token)
+  end
+
+  describe :new do
+    subject { @userstream }
+    it { should be_instance_of Userstream }
   end
 end

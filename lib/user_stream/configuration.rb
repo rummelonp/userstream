7590c8b48fba937fad7b5bf66fe379e3d0b647c2
Kazuya Takeshima <mail@mitukiii.jp>
2012-01-07 13:05:46 +0900

    Sort options keys.

--- lib/user_stream/configuration.rb
+++ lib/user_stream/configuration.rb
@@ -9,8 +9,8 @@ module UserStream
       :consumer_secret,
       :oauth_token,
       :oauth_token_secret,
-      :user_agent,
       :endpoint,
+      :user_agent,
     ].freeze
 
     # By default, don't set a consumer key
@@ -25,14 +25,14 @@ module UserStream
     # By default, don't set an oauth token secret
     DEFAULT_OAUTH_TOKEN_SECRET = nil
 
-    # The user agent that will be sent to the API endpoint if none is set
-    DEFAULT_USER_AGENT = "UserStream/#{VERSION} (http://github.com/mitukiii/userstream)".freeze
-
     # The endpoint that will be used to connect if none is set
     #
     # @note Specify a different API version, or use a Twitter-compatible endpoint.
     # @see https://dev.twitter.com/docs/streaming-api/user-streams
     DEFAULT_ENDPOINT = 'https://userstream.twitter.com/'.freeze
+
+    # The user agent that will be sent to the API endpoint if none is set
+    DEFAULT_USER_AGENT = "UserStream/#{VERSION} (http://github.com/mitukiii/userstream)".freeze
 
     # @private
     attr_accessor *OPTIONS_KEYS
@@ -61,8 +61,8 @@ module UserStream
       self.consumer_secret    = DEFAULT_CONSUMER_SECRET
       self.oauth_token        = DEFAULT_OAUTH_TOKEN
       self.oauth_token_secret = DEFAULT_OAUTH_TOKEN_SECRET
-      self.user_agent         = DEFAULT_USER_AGENT
       self.endpoint           = DEFAULT_ENDPOINT
+      self.user_agent         = DEFAULT_USER_AGENT
     end
   end
 end
